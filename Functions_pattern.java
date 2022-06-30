9689a4f18cde06e7d59c50fff043ec90c8552dd1
gulshan0201 <84889171+gulshan0201@users.noreply.github.com>
2022-06-30 22:54:45 +0530

    Create Functions_pattern.java

---
+++ Functions_pattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class functions {
+
+  public static Scanner sc = new Scanner(System.in);
+
+  public static void main(String args[]){
+    int noOfLines = sc.nextInt();
+    patter(noOfLines);
+  }
+
+  public static void patter(int noOfLines){
+    for(int i=1; i<=noOfLines; i++){
+      singleLine(i);
+      System.out.println();
+    }
+  }
+
+  public static void singleLine(int noofstars){
+    for(int i=1; i<=noofstars; i++){
+      System.out.print("* ");
+    }
+  }
+}
