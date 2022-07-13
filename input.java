7bb8f54ff9c48e3ed824bd4900b5428f97aa0b43
gulshan0201 <84889171+gulshan0201@users.noreply.github.com>
2022-07-13 16:52:54 +0530

    Create input.java

---
+++ input.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class solution {
+    
+    public static Scanner sc = new Scanner(System.in);
+    
+    public static void  main(String args[]){
+        int a = sc.nextInt();
+        Double d = sc.nextDouble();
+        String s = sc.nextLine();
+        
+        System.out.println("String : " + s);
+        System.out.println("Double : " + d);
+        System.out.println("String : " + s);
+    }
+}
